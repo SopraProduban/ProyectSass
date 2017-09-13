35084f757e2754d2a606108b42bb7402ca2f9018
VALLEJO GUTIERREZ Monica <monica.vallejo@soprasteria.com>
2017-09-13 10:21:51 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponenteComponent } from './componente/componente.component';
+import { Componente2Component } from './componente2/componente2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteComponent,
+    Componente2Component
   ],
   imports: [
     BrowserModule
